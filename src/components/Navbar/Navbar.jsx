711db96c894dbcebbf334e8f88549b92ac7e43b6
Sebastian Perez <sebasperezr@gmail.com>
2022-12-20 02:52:04 -0500

    Revert "Delete src/components directory"
    This reverts commit c19a2486f97195ea639cd3fa83fb233159f6d948.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Navbar.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import CartWidget from "../cartWidget/CartWidget.jsx";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <Link className="navbar-brand" to="/">
+          Morita
+        </Link>
+        <div className="navbar-toggler cart">
+          <Link to={"/cart"}>
+            <CartWidget />
+          </Link>
+        </div>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/category/1">
+                ACCESORIOS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/category/2">
+                CEJAS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/category/3">
+                LABIOS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/category/4">
+                CUIDADO DE PIE
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/category/5">
+                OJOS
+              </Link>
+            </li>
+          </ul>
+          <div className="d-flex">
+            <Link to={"/cart"}>
+              <CartWidget />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
