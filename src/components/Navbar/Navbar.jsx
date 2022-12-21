d7801acd1631384040c3a4afce41d20139d7eeac
Sebastian Perez <sebasperezr@gmail.com>
2022-12-20 20:22:41 -0500

    Revert "Add navbar fixed"
    This reverts commit 7eee21637bf77707b69c0ef3c59d877f3caccb2e.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg fixed-top">
+    <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
         <button
           className="navbar-toggler"
